5d4fb833f0989b8c800822a5a1799b0187db3a5a
akashmony01 <akashmonya88@gmail.com>
2021-03-17 20:09:19 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        grandRoyal: ['Grand Royal'],
+        goldplay: ['Goldplay'],
+      },
+      colors: {
+        'bgColor': '#FFF9E4',
+        'primary': '#121212',
+        'secondery': '#368F8B',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
